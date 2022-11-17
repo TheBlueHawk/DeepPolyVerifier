2cbc0f7e690ee5521a30878f4dcbaf48dbc0aa84
FreeJ99 <jenkoslobodan@gmail.com>
2022-11-17 14:16:00 +0100

    Implemented expand_abstract_shape that expands the compacted representation of ReLU.

--- code/abstract_shape.py
+++ code/abstract_shape.py
@@ -21,6 +21,26 @@ class AbstractShape:
     def __str__(self):
         return f"{self.y_greater}\n{self.y_less}\n{self.lower}\n{self.upper}"
 
+def expand_abstract_shape(abstract_shape):
+    """Rerepresents a compact relu abstract shape wrt. all neurons in the prevoious layer"""
+    
+    intercept_greater = torch.zeros_like(abstract_shape.y_greater)
+    weights_greater = abstract_shape.y_greater.flatten()
+    weights_greater_expanded = torch.diag(weights_greater)
+    y_greater = torch.cat([intercept_greater, weights_greater_expanded], axis=1)
+
+    intercept_less = abstract_shape.y_less[:, 0:1]
+    weights_less = abstract_shape.y_less[:, 1]
+    weights_less_expanded = torch.diag(weights_less)
+    y_less = torch.cat([intercept_less, weights_less_expanded], axis=1)
+
+    return AbstractShape(
+        y_greater, 
+        y_less, 
+        abstract_shape.lower, 
+        abstract_shape.upper)
+    
+
 
 def create_abstract_input_shape(inputs, eps):
     return AbstractShape(
@@ -29,3 +49,9 @@ def create_abstract_input_shape(inputs, eps):
         lower=torch.clamp(inputs-eps, 0, 1),
         upper=torch.clamp(inputs+eps, 0, 1)
     )
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ tests/abstract_shape_test.py
@@ -0,0 +1,20 @@
+import torch
+import sys
+sys.path.append("./code")
+
+from abstract_shape import AbstractShape, expand_abstract_shape
+
+def test_expand_abstract_shape():
+    abstract_shape = AbstractShape(
+        torch.tensor([[1], [2]]),
+        torch.tensor([[3, 4], [5, 6]]),
+        torch.tensor([7, 8]),
+        torch.tensor([9, 10])
+    )
+
+    out = expand_abstract_shape(abstract_shape)
+    
+    assert torch.allclose(out.y_greater, torch.tensor([[0, 1, 0], [0, 0, 2]]))
+    assert torch.allclose(out.y_less, torch.tensor([[3, 4, 0], [5, 0, 6]]))
+    assert torch.allclose(out.lower, torch.tensor([7, 8]))
+    assert torch.allclose(out.upper, torch.tensor([9, 10]))
