fix: add normalization, fix Abstractresnets layers

code/abstract_networks.py
@@ -13,6 +13,7 @@ from transformers import (
     AbstractResidualSum,
 )
 from anet_checkers import ANetChecker
+from networks import NormalizedResnet
 
 
 def recompute_bounds_linear(first_ashape, curr_ashape):
@@ -636,20 +637,23 @@ class AbstractNet7(AbstractNetwork):
 
 
 class AbstractNet8(AbstractNetwork):
-    def __init__(self, net, checker) -> None:
-        self.conv1 = AbstractConvolution(net.layers[1])
+    def __init__(self, net: NormalizedResnet, checker) -> None:
+        self.normalize = AbstractNormalize(net.normalization)
+        resnet = net.resnet
+
+        self.conv1 = AbstractConvolution(resnet[0])
         self.relu1 = AbstractReLU()
 
-        blockLayers1: torch.nn.Sequential = net.layers[3][0]
-        self.block1 = AbstractBlockSubnet(blockLayers1.layer[0])
+        blockLayers1: torch.nn.Sequential = resnet[2]
+        self.block1 = AbstractBlockSubnet(blockLayers1[0], checker)
         self.relu2 = AbstractReLU()
-        self.block2 = AbstractBlockSubnet(blockLayers1.layer[3])
+        self.block2 = AbstractBlockSubnet(blockLayers1[2], checker)
         self.relu3 = AbstractReLU()
 
         self.flatten = AbstractFlatten()
-        self.lin1 = AbstractLinear(net.layers[5])
+        self.lin1 = AbstractLinear(resnet[4])
         self.relu4 = AbstractReLU()
-        self.lin2 = AbstractLinear(net.layers[7])
+        self.lin2 = AbstractLinear(resnet[6])
         self.final_atransformer = None  # built in forward
 
         self.checker = checker
@@ -658,6 +662,10 @@ class AbstractNet8(AbstractNetwork):
         self.final_atransformer = self.buildFinalLayer(true_label, N)
         prev_abstract_shapes = []
         self.checker.check_next(abstract_shape)
+
+        abstract_shape = self.normalize.forward(abstract_shape)
+        self.checker.check_next(abstract_shape)
+        prev_abstract_shapes.append(abstract_shape)
 
         abstract_shape = self.conv1.forward(abstract_shape)
         self.checker.check_next(abstract_shape)
@@ -735,23 +743,26 @@ class AbstractNet8(AbstractNetwork):
 
 
 class AbstractNet9(AbstractNetwork):
-    def __init__(self, net, checker) -> None:
-        self.conv1 = AbstractConvolution(net.layers[1])
-        self.bn1 = AbstractBatchNorm(net.layers[2])
+    def __init__(self, net: NormalizedResnet, checker) -> None:
+        self.normalize = AbstractNormalize(net.normalization)
+        resnet: ResNet = net.resnet
+
+        self.conv1 = AbstractConvolution(resnet[0])
+        self.bn1 = AbstractBatchNorm(resnet[1])
         self.relu1 = AbstractReLU()
 
-        blockLayers1: torch.nn.Sequential = net.layers[4][0]
-        self.block1 = AbstractBlockSubnet(blockLayers1.layer[0])
+        blockLayers1: torch.nn.Sequential = resnet[3]
+        self.block1 = AbstractBlockSubnet(blockLayers1[0], checker)
         self.relu2 = AbstractReLU()
 
-        blockLayers2: torch.nn.Sequential = net.layers[4][0]
-        self.block2 = AbstractBlockSubnet(blockLayers2.layer[0])
+        blockLayers2: torch.nn.Sequential = resnet[4]
+        self.block2 = AbstractBlockSubnet(blockLayers2[0], checker)
         self.relu3 = AbstractReLU()
 
         self.flatten = AbstractFlatten()
-        self.lin1 = AbstractLinear(net.layers[6])
+        self.lin1 = AbstractLinear(resnet[6])
         self.relu4 = AbstractReLU()
-        self.lin2 = AbstractLinear(net.layers[8])
+        self.lin2 = AbstractLinear(resnet[8])
         self.final_atransformer = None  # built in forward
 
         self.checker = checker
@@ -760,6 +771,10 @@ class AbstractNet9(AbstractNetwork):
         self.final_atransformer = self.buildFinalLayer(true_label, N)
         prev_abstract_shapes = []
         self.checker.check_next(abstract_shape)
+
+        abstract_shape = self.normalize.forward(abstract_shape)
+        self.checker.check_next(abstract_shape)
+        prev_abstract_shapes.append(abstract_shape)
 
         abstract_shape = self.conv1.forward(abstract_shape)
         self.checker.check_next(abstract_shape)
@@ -841,26 +856,29 @@ class AbstractNet9(AbstractNetwork):
 
 
 class AbstractNet10(AbstractNetwork):
-    def __init__(self, net, checker) -> None:
-        self.conv1 = AbstractConvolution(net.layers[1])
+    def __init__(self, net: NormalizedResnet, checker) -> None:
+        self.normalize = AbstractNormalize(net.normalization)
+        resnet: ResNet = net.resnet
+
+        self.conv1 = AbstractConvolution(resnet[0])
         self.relu1 = AbstractReLU()
 
-        blockLayers1: torch.nn.Sequential = net.layers[3][0]
-        self.block1 = AbstractBlockSubnet(blockLayers1.layer[0])
+        blockLayers1: torch.nn.Sequential = resnet[2]
+        self.block1 = AbstractBlockSubnet(blockLayers1[0], checker)
         self.relu2 = AbstractReLU()
-        self.block2 = AbstractBlockSubnet(blockLayers1.layer[3])
+        self.block2 = AbstractBlockSubnet(blockLayers1[2], checker)
         self.relu3 = AbstractReLU()
 
-        blockLayers2: torch.nn.Sequential = net.layers[4][0]
-        self.block3 = AbstractBlockSubnet(blockLayers2.layer[0])
+        blockLayers2: torch.nn.Sequential = resnet[3]
+        self.block3 = AbstractBlockSubnet(blockLayers2[0], checker)
         self.relu4 = AbstractReLU()
-        self.block4 = AbstractBlockSubnet(blockLayers2.layer[3])
+        self.block4 = AbstractBlockSubnet(blockLayers2[2], checker)
         self.relu5 = AbstractReLU()
 
         self.flatten = AbstractFlatten()
-        self.lin1 = AbstractLinear(net.layers[6])
+        self.lin1 = AbstractLinear(resnet[5])
         self.relu6 = AbstractReLU()
-        self.lin2 = AbstractLinear(net.layers[8])
+        self.lin2 = AbstractLinear(resnet[7])
         self.final_atransformer = None  # built in forward
 
         self.checker = checker
@@ -869,6 +887,10 @@ class AbstractNet10(AbstractNetwork):
         self.final_atransformer = self.buildFinalLayer(true_label, N)
         prev_abstract_shapes = []
         self.checker.check_next(abstract_shape)
+
+        abstract_shape = self.normalize.forward(abstract_shape)
+        self.checker.check_next(abstract_shape)
+        prev_abstract_shapes.append(abstract_shape)
 
         abstract_shape = self.conv1.forward(abstract_shape)
         self.checker.check_next(abstract_shape)

code/deep_poly_verifier.py
@@ -4,12 +4,15 @@ from torch import Tensor
 from abstract_shape import create_abstract_input_shape, AbstractShape, weightedLoss
 from abstract_networks import (
     AbstractNet1,
+    AbstractNet10,
     AbstractNet2,
     AbstractNet3,
     AbstractNet4,
     AbstractNet5,
     AbstractNet6,
     AbstractNet7,
+    AbstractNet8,
+    AbstractNet9,
     AbstractNetwork,
 )
 from anet_checkers import ANetChecker, DummyANetChecker, InclusionANetChecker
@@ -24,6 +27,9 @@ def get_anet_class_from_name(net_name) -> AbstractNetwork:
         "net5": AbstractNet5,
         "net6": AbstractNet6,
         "net7": AbstractNet7,
+        "net8": AbstractNet8,
+        "net9": AbstractNet9,
+        "net10": AbstractNet10,
     }
     return abstract_nets[net_name]
 
@@ -40,6 +46,7 @@ def get_anet_class_from_name(net_name) -> AbstractNetwork:
 #     }
 #     return checkers[net_name]
 
+
 def get_checker_class_from_name(net_name) -> ANetChecker:
     checkers = {
         "net1": InclusionANetChecker,
@@ -49,6 +56,9 @@ def get_checker_class_from_name(net_name) -> ANetChecker:
         "net5": InclusionANetChecker,
         "net6": InclusionANetChecker,
         "net7": InclusionANetChecker,
+        "net8": DummyANetChecker,
+        "net9": DummyANetChecker,
+        "net10": DummyANetChecker,
     }
     return checkers[net_name]
 
@@ -85,20 +95,22 @@ class DeepPolyVerifier:
                 # print(f"Max violation:\t {final_abstract_shape.lower.min()}")
                 if verifyFinalShape(final_abstract_shape):
                     return True
-                
+
                 self.checker.reset(inputs)
 
                 # Do just one step and then recompute the output
                 # Alternatively could do multiple step with the existing mapping
                 # from input neurons to bounds
-                
+
                 alphas = self.abstract_net.get_alphas()
                 optim = torch.optim.Adam(alphas, lr=self.LR)
                 optim.zero_grad()
                 loss = weightedLoss(final_abstract_shape.lower, self.gamma)
                 loss.backward()
                 optim.step()
-                alphas_clamped = [a.clamp(0, 1).detach().requires_grad_() for a in alphas]
+                alphas_clamped = [
+                    a.clamp(0, 1).detach().requires_grad_() for a in alphas
+                ]
                 self.abstract_net.set_alphas(alphas_clamped)
 
             alphas = self.abstract_net.get_alphas()

code/transformers.py
@@ -226,7 +226,6 @@ class AbstractConvolution:
 
     def _init_from_layer(self, convLayer):
         self.kernel: Tensor = convLayer.weight.data  # [c_out, k_h, k_w, c_in]
-        self.bias = convLayer.bias.data  # [c_out]
         assert self.kernel.shape[3] == self.kernel.shape[2]
         self.k = self.kernel.shape[3]
         assert convLayer.kernel_size == (self.k, self.k)
@@ -238,6 +237,10 @@ class AbstractConvolution:
         self.padding: int = convLayer.padding[0]
         assert convLayer.dilation[0] == convLayer.dilation[1]
         self.dilation: int = convLayer.dilation[0]
+        if convLayer.bias is None:
+            self.bias = Tensor([0] * self.c_out)  # [c_out]
+        else:
+            self.bias = convLayer.bias.data  # [c_out]
         self.N = None
 
     def forward(
@@ -298,18 +301,20 @@ class AbstractConvolution:
             k_flat > 0, u_unfold.swapdims(-1, -2), l_unfold.swapdims(-1, -2)
         )  # [self.c_out, n_possible_kernel_positions, self.w * self.h * self.c_in]
 
+        new_l = torch.sum(l_cube * k_flat, dim=-1).view(
+            self.c_out, h_out, w_out
+        )  # [self.c_out, h_out, w_out]
+
+        new_u = torch.sum(u_cube * k_flat, dim=-1).view(
+            self.c_out, h_out, w_out
+        )  # [self.c_out, h_out, w_out]
+
         expanded_bias = self.bias.reshape(-1, 1, 1).repeat(
             1,
             self.N,
             self.N,
         )  # [c_out, N, N]
-        new_l = torch.sum(l_cube * k_flat, dim=-1).view(
-            self.c_out, h_out, w_out
-        )  # [self.c_out, h_out, w_out]
         new_l += expanded_bias
-        new_u = torch.sum(u_cube * k_flat, dim=-1).view(
-            self.c_out, h_out, w_out
-        )  # [self.c_out, h_out, w_out]
         new_u += expanded_bias
 
         y_greater_one_neuron = (
@@ -350,19 +355,6 @@ class AbstractConvolution:
 
 class AbstractResidualSum:
     def __init__(self, *args) -> None:
-        if isinstance(args[0], nn.BasicBlock):
-            self._init_from_layer(*args)
-
-        elif isinstance(args[0], torch.Tensor):
-            pass
-            # self._init_from_tensor(*args)
-
-        else:
-            raise Exception(
-                "Invalid arguments passed to the initializer of AbstractLinear"
-            )
-
-    def _init_from_layer(self):
         pass
 
     # TODO: probably change a,b to ConvAS